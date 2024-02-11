781eb7170f5b2832bae16ac091aa6d1b08ca7fe6
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-11 10:51:59 +0000

    ConversionOfDatatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+console.log("Conversion Operations");
+
+//let score="33" =>33
+//let score="33abc"; =>Nan Not an Number
+//let score=null;
+//let score=undefined;
+//let score=true;  =>1
+//let score=false; =>0
+//   let score="rishab";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//let isLoggedIn=0;//false return
+//let isLoggedIn=1;//true return
+//let isLoggedIn="";//false return
+let isLoggedIn= "rishab";//true return
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
