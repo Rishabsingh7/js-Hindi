b8b45bea818154fb971bae6a558e2d920f5d63e7
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-11 11:28:42 +0000

    add conversionOperation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -27,3 +27,47 @@ let someNumber=33
 let stringNumber=String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// ********************* Operations ************************
+
+let value=3;
+let negValue=-value
+console.log(negValue);  //output -3
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);2^3
+console.log(2/2);
+console.log(4%4);
+
+let str1="hello";
+let str2=" rishab";
+let str3=str1+str2;
+console.log(str3);
+
+console.log("2"+4);//output 24
+console.log(2+"4");//output 24
+console.log("2"+2+2);//output 222
+console.log(2+2+"2");//output 42
+console.log(2+"2"+2);//output 222
+
+console.log((3+4)*5%3);
+console.log(true);//output true
+console.log(+true);//1
+console.log(-true);//-1
+console.log(+"");//0
+
+
+let num1,num2,num3
+num1=num2=num3=2+2;
+console.table(["num1= "+num1,"num2= "+num2,"num2= "+num3]);
+
+let gameCounter=100;
+++gameCounter;//prefix
+console.log(gameCounter);
+let gamecounter=100;
+gamecounter++;//postfix
+console.log(gamecounter);
