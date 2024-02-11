3a41febdd5eba561f90f0581e95b1ad971b890c7
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-11 05:16:38 +0000

    container config done

---
+++ .devcontainer/01_basics/test.js
@@ -0,0 +1,6 @@
+console.log("hello Worlds");
+console.log("to kaise ho aap log");
+console.log("hello baiyo");
+console.log("hello Worlds");
+console.log("hello Worlds");
+console.log("hello Worlds");
