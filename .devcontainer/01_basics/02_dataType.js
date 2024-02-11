3f1d77ab0ebebc6ad0ca78013e7498292f5ce057
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-11 10:20:43 +0000

    add DataTypes

---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,26 @@
+console.log("DataType");
+
+"use strict";//treat all JS code as newer version
+
+//alert(3+3);// we are using nodejs, not browser
+console.log(3+3); //code readability should be high
+console.log("rishab")
+let name="rishab";//String dataType
+let age=20;//  dataType
+let isLoggedIn=false;//boolean Datatype
+let state=null;
+/*  DataTypes
+number
+bigint
+String =>""
+boolean => true/false
+null => standalone value
+undefined => value is not assigned
+symbol => for unique
+object 
+*/
+
+console.log(typeof "rishab");//output string 
+console.log(typeof null);//output object
+console.log(typeof age);//output number
+console.log(typeof undefined);//Output undefined
