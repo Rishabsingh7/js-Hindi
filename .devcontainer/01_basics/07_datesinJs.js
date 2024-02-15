4625e17bd8b188bf713617b550a5206fe81d7c03
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-15 20:34:38 +0000

    add

---
+++ .devcontainer/01_basics/07_datesinJs.js
@@ -0,0 +1,15 @@
+//Dates
+
+// let myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+// console.log(typeof myDate);
+
+//let myCreatedDate=new Date(2001,9,9);
+let myCreatedDate=new Date(2001,9,9,5,6);
+//console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleDateString());
