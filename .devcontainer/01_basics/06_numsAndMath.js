aa878c9f2dd6f5f6e6b3943e64b02f555a43b404
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-15 20:17:12 +0000

    nums and maths

--- .devcontainer/01_basics/06_numsAndMath.js
+++ .devcontainer/01_basics/06_numsAndMath.js
@@ -33,6 +33,6 @@ console.log((Math.random()*10)+1);
 console.log(Math.floor(Math.random()*10)+1);
 
 
-const min=10;
-const max=20;
+const min=1;
+const max=100;
 console.log(Math.floor(Math.random()*(max-min+1))+min);
