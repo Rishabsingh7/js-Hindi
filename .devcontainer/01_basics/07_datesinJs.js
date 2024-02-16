cf20800e84f9d7c098a07d76417bcc5579d75689
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-16 04:58:50 +0000

    complete the folder

--- .devcontainer/01_basics/07_datesinJs.js
+++ .devcontainer/01_basics/07_datesinJs.js
@@ -10,6 +10,34 @@
 // console.log(typeof myDate);
 
 //let myCreatedDate=new Date(2001,9,9);
-let myCreatedDate=new Date(2001,9,9,5,6);
+//let myCreatedDate=new Date(2001,9,9,5,6);
 //console.log(myCreatedDate.toDateString());
-console.log(myCreatedDate.toLocaleDateString());
+//console.log(myCreatedDate.toLocaleDateString());
+
+//let myCreatedDate=new Date("2023-01-14")//yy/mm/dd
+let myCreatedDate=new Date("01-14-2023")//mm/dd/yy
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));//convert in seconds
+
+// let newDate=new Date();
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getHours());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getSeconds());
+// console.log(newDate.getMinutes());
+
+//mainly we write in program on project 
+
+//`${newDate.getDay()} and the time`
+
+// newDate.toLocaleString('default',{
+//     weekday:"long",
+// })
