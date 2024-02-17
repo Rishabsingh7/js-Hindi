09f24bd28d5ab2f8a69b83b790b5b290f56c195c
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-17 19:44:53 +0000

    completed Arrow Functions

--- .devcontainer/01_basics/14_ArrowFunctions.js
+++ .devcontainer/01_basics/14_ArrowFunctions.js
@@ -5,6 +5,51 @@ const user={
     price:999,
     welcomeMessage:function(){
         console.log(`${this.userName}, welcome to website`);
+        //console.log(this);
     }
-
 }
+// user.welcomeMessage();//Its default return value 
+// user.userName="shalu"
+// user.welcomeMessage();
+
+//console.log(this);//return empty objects {}
+
+//Note browser ke under global object hai wo Window Objects hai  
+
+
+// function chai(){
+//     let userName="rishab"
+//     console.log(this.userName);
+// }
+// chai();
+
+// const chai=function(){
+//     let userName="rishab"
+//     console.log(this.userName);
+// }
+// chai();
+
+
+//ArrowFunction
+const chai=()=>{
+    let userName="rishab"
+    //console.log(this);
+}
+chai();
+
+//Arrow Functions Basica syntax
+// const addTwo=(num1,num2)=>{
+//    return num1+num2; //Explicite return 
+// }
+// console.log(addTwo(2,3));
+
+//Implicit return
+//const addTwo=(num1,num2)=> num1+num2;
+//const addTwo=(num1,num2) =>(num1+num2);
+//console.log(addTwo(2,3));
+
+//when we return object
+const addTwo=(num1,num2) =>({userName:"rishab"})
+console.log(addTwo(3,4));
+
+const myArray=[2,4,5,6,7]
