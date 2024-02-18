d1027d206b4085c6944f2b27b611c847503725b2
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-18 19:37:09 +0000

    add the loops concept

--- .devcontainer/01_basics/19_Loops.js
+++ .devcontainer/01_basics/19_Loops.js
@@ -1,5 +1,22 @@
 console.log("Loops/Iteration");
 
-for (let i = 1; i <=10; i++) {
-    console.log(i);
+// for (let i = 1; i <=10; i++) {
+//     console.log(i);
+// }
+// for (let i = 1; i <=10; i++) {
+//     if(i==5){
+//         console.log("5 is best number");
+//     }
+//     console.log(i);
+// }
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop value:${i}`);
+    for(let j=0;j<=10;j++){
+        //console.log(`Inner loops value: ${j} and inner loops ${i}`)
+        console.log((i+'*'+j+'='+i*j));
+
+    }
+    
 }
