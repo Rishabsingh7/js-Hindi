538de32629fab40823eff53bf1936f2a102c1c57
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-18 19:14:50 +0000

    completed switch case

--- .devcontainer/01_basics/18_Switch.js
+++ .devcontainer/01_basics/18_Switch.js
@@ -52,3 +52,51 @@ switch (month) {
     default:
         break;
 }
+
+//+++++++++ Truthy value
+const userEmail="rishab@.gmail.com";
+if(userEmail){
+    console.log("Got user email");
+} 
+else{
+    console.log("Don't have user email");
+}
+
+//falsy values
+// false,0,-0,BinInt 0n, "",null,undefined,NaN
+
+//Truthy value
+// True,1,"0",'false'," ",[],{},function(){},
+
+const useremail=[];
+if(useremail.length===0){
+    console.log("Array is empty");
+}
+
+const emptyObj={}
+if(Object.keys(emptyObj).length===0){
+    console.log("Object is empty");
+}
+
+//Some important keys
+//false==0;//return true
+//false=='';//return true
+//0=='';//return true
+
+//Nullish Coalescing Opertator (??):
+//Its apply only two keys 1 Null 2 Undefined
+
+let val1;
+//val1=5??10;//return 5
+//val1=null??10;//return 10
+//val1=undefined??15;//return 15
+//val1=null??undefined;//return Undefined
+//val1=undefined??null;//return null
+val1=null??10??20;//return 10
+console.log(val1);
+
+// Terniary Operator
+
+//condition ? true : false
+const iceTeaPrice=100;
+iceTeaPrice<=80?console.log("less than 80"):console.log("more than 80")
