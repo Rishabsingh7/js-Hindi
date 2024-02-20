8a6a40709755188e1e11ff826350dc9042edda51
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-20 17:56:42 +0000

    continue

--- .devcontainer/01_basics/19_Loops.js
+++ .devcontainer/01_basics/19_Loops.js
@@ -11,12 +11,12 @@ console.log("Loops/Iteration");
 // }
 
 
-for (let i = 0; i <= 10; i++) {
-    console.log(`Outer loop value:${i}`);
-    for(let j=0;j<=10;j++){
-        //console.log(`Inner loops value: ${j} and inner loops ${i}`)
-        console.log((i+'*'+j+'='+i*j));
+//for (let i = 1; i <= 10; i++) {
+  //  console.log(`Outer loop value:${i}`);
+    //for(let j=1;j<=10;j++){
+       // console.log(`Inner loops value: ${j} and inner loops ${i}`)
+      //  console.log((i+'*'+j+'='+i*j));
 
-    }
+  //  }
     
-}
+//}
