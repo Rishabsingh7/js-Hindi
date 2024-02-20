129964f6749f35d613ca90e45839680257bd638b
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-20 18:15:32 +0000

    complete break statement

--- .devcontainer/01_basics/19_Loops.js
+++ .devcontainer/01_basics/19_Loops.js
@@ -20,3 +20,22 @@ console.log("Loops/Iteration");
   //  }
     
 //}
+
+
+let myArray=["flash","batman","superman","rishab","mohan"]
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];   
+    console.log(element);
+}
+
+//break and continue
+
+for (let index = 1; index <=20; index++) {
+    if(index==5){
+    console.log(`Detected 5`);
+    break;    
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
