5f3f2ae88ac760b9c2a4aaede185cb589c70e014
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-21 18:05:22 +0000

    recode

--- .devcontainer/01_basics/20_ForOfAndForEachLoops.js
+++ .devcontainer/01_basics/20_ForOfAndForEachLoops.js
@@ -1,6 +1,7 @@
 console.log("For Of Loops and For Each Loops");
 
-//
+//["","",""]
+//[{},{},{}]
 
 const arr=[1,2,3,4,5,6]
 for (const num of arr) {
@@ -14,15 +15,15 @@ for (const greet of greetings) {
 }
 
 //Maps in JavaScripts
-// const map=new Map()
-// map.set('IN',"India");
-// map.set('USA',"United States of America");
-// map.set('Fr',"France");
+const map=new Map()
+map.set('IN',"India");
+map.set('USA',"United States of America");
+map.set('Fr',"France");
 
-//console.log(map);
-// for(const [key,value] of map){
-// console.log(key,':-',value);
-// }
+console.log(map);
+for(const [key,value] of map){
+console.log(key,':-',value);
+}
 
 const myObject={
     'game1':'NFS',
@@ -72,12 +73,12 @@ const myObjects={
 // })
 
 function printMe(item){
-    console.log(item);
+    //console.log(item);
 }
 //coding.forEach(printMe);
 
 coding.forEach((item,index,arr)=>{
-    console.log(item,index,arr);
+    //console.log(item,index,arr);
 })
 
 const myCoding=[
@@ -104,6 +105,6 @@ const myCoding=[
 ]
 
 myCoding.forEach((item)=>{
-    console.log(item.languageName);
-    console.log(item.languageFileName);
+   // console.log(item.languageName);
+   // console.log(item.languageFileName);
 })
