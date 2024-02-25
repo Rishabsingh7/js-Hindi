ebf029695ba37eb390035a66b6eb71aa177f1ac6
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-25 18:29:01 +0000

    complete Async code in JS

---
+++ .devcontainer/03_EventsInJS/AsyncCode.js
@@ -0,0 +1,27 @@
+//Async Code in JavaScript
+
+/**
+   JavaScript 
+             Syschronous 
+             Single threaded 
+
+             Default Java Script
+
+    Execution Context 
+      execute one line of code at a time      
+      console.log -> 1  
+      console.log -> 2
+                each operation waits for the last one to complete before executing   
+          
+    CALL Stack        Memory Heap
+
+
+    Blocking Code Vs Non Blocking code 
+    1.Blocking Code 
+    Block the flow of Program
+    Read File Sync
+   
+    2.Non Blocking code
+    Does not block execution 
+    Read File Async
+ */
