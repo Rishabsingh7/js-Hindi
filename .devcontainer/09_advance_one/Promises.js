3c70dbdd0eca2ffea14df393b2a9665c8db5eb6f
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-02-26 18:37:40 +0000

    Add some topics

--- .devcontainer/09_advance_one/Promises.js
+++ .devcontainer/09_advance_one/Promises.js
@@ -1,1 +1,99 @@
-const promiseOne=new Promise()
+const promiseOne=new Promise(function(resolve, reject) {
+  //DO an async task
+  //DB calls, cryptography, network
+  setTimeout(function(){
+    console.log('Async task is compelete');
+    resolve()
+  },2000)
+  
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    },2000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+
+//Third Promised
+const promiseThree=new Promise(function(resolve,reject){
+   setTimeout(function(){
+      resolve({username:"rishab",email:"rishab@gmail.com"})
+   },2000)
+})
+
+promiseThree.then(function(user){
+    console.log(user)
+})
+
+
+//Promise Four 
+const promiseFour=new Promise(function(resolve,reject){
+    setTimeout(function() {
+        //let error=false;
+        let error=true;
+        if(!error){
+            resolve({username:"rishab",password:"123"})
+        }
+        else{
+            reject('ERROR: Something went wrong')
+        }
+    }, 2000);
+})
+ promiseFour
+ .then((user)=>{
+    console.log(user);
+    return user.username;
+})
+.then((username)=>{
+ console.log(username);
+})
+.catch(function(error){
+    console.log(error)
+}).finally(() => console.log("The Promise is resolved or rejected "))
+
+
+//Promise Five 
+const promiseFive =new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true
+        //let error=false
+        if(!error){
+            resolve({username:"JavaScript",password:"123"})
+        } else{
+            reject('ERROR:JS went wrong')
+        }
+    },2000)
+})
+
+async function consumePromiseFive(){
+   try{
+    const response=await promiseFive
+    console.log(response);
+   }catch(error){
+    console.log(error)
+   }
+}
+consumePromiseFive()
+
+
+//Next Class Topics
+async function getAllUser(){
+    try{
+        const response=fetch('https://api.github.com/users/Rishabsingh7')
+       // console.log(response);
+     const data=await response.json()
+     console.log(data);
+    }catch(error){
+        console.log("E: ",error);
+    }
+}
+getAllUser()
