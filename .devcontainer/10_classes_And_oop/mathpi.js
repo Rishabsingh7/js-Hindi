8f70eef1c10005d8e3af20c7805a55ca7992cbb6
Rishab Singh <118653979+Rishabsingh7@users.noreply.github.com>
2024-03-07 08:35:43 +0000

    add some correction in files

--- .devcontainer/10_classes_And_oop/mathpi.js
+++ .devcontainer/10_classes_And_oop/mathpi.js
@@ -4,48 +4,44 @@
 //getOwnPropertyDescriptor it is tells something Objects hidden 
 
 const descripter=Object.getOwnPropertyDescriptor(Math,"PI")
-console.log(descripter);
+//console.log(descripter);
 
 
 //console.log(Math.PI);
 //Math.PI=5
 //console.log(Math.PI);
 
-const chai={
+ const chai={
     name:'ginger chai',
     price:250,
     isAvailable:true,
 
-    orderChai:function(){
-        console.log("Chai nahi bani")
-    }
+     orderChai:function(){
+         console.log("Chai nahi bani");//code faat gaya 
+     }
 
 }
-//console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+//console.log(chai);
 
-// Object.defineProperty(chai,'name',{writable:false,
-//     enumerable:false
-// })
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
 
-// console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+ 
+Object.defineProperty(chai,'name',{writable:false,
+    //writable:false,
+    enumerable:true,
+    //configurable:false
 
+ })
 
-for(let [key,value] of Object.enteries(chai)){
-    if(typeof value !=='function'){
-        console.log(`${key} : ${value}`);
-    }
-
-
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
 
 
 
+for(let [key,value] of Object.entries(chai)){
+     if(typeof value !=='function'){
+         console.log(`${key} : ${value}`);
+ }
+ }
 
-
-
-
-
-
-
-
-    
-}
+ //Home Work 
+ //Object Iterate properties javaScript display
